98bf06cfa0dd84b979420da5614e3515bbac8528
jabbyjim <jabbyjim@gmail.com>
2022-05-11 19:08:04 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "jv,q%7ciij2?V$i+nTh/":
+            case "Me":return img`
+. . . . . . . . . . . . . . . . 
+. f f f f f f f f f f f f f f . 
+. f . . . . . . . . . . . . f . 
+. f . . . . . . . . . . . . f . 
+. f . . . e e e e e e . . . f . 
+. f . . e d d d d d d e . . f . 
+. f . . e d f d d f d e . . f . 
+. f . . d d d d d d d d . . f . 
+. f . . d d d d d d d d . . f . 
+. f . . . d d f f d d . . . f . 
+. f . . . d d d d d d . . . f . 
+. f . . 5 5 5 5 5 5 5 5 . . f . 
+. f . 5 5 5 5 8 8 5 5 5 . . f . 
+. f . 5 5 5 8 8 8 8 5 5 5 . f . 
+. f . 5 5 5 5 8 8 5 5 5 5 . f . 
+. f f f f f f f f f f f f f f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
